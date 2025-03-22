e8521516ef4f014da0c82f71fd0165f5caebe501
rabby159 <ablazerabbi159@gmail.com>
2025-03-22 16:14:57 +0600

    blp project

--- world-tour/src/components/Countries/Countries.jsx
+++ world-tour/src/components/Countries/Countries.jsx
@@ -2,20 +2,19 @@ import React, { useEffect, useState } from "react";
 import Country from "./Country";
 
 const Countries = () => {
+  const [countries, setCountries] = useState([]);
 
-    const [countries, setCountries] = useState([]);
-
-    useEffect(() => {
-        fetch('https://restcountries.com/v3.1/all')
-            .then(res => res.json())
-            .then(data => setCountries(data))
-    }, [])
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
   return (
     <div>
-          <p>All Country Here!</p>
-          {
-              countries.map(country => <Country country={country}></Country>)
-          }
+      <p>All Country Here! It's total {countries.length}</p>
+      {countries.map((country) => (
+        <Country key={country.cca2} country={country}></Country>
+      ))}
     </div>
   );
 };
