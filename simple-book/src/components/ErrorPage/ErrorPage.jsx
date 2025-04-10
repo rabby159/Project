581d0bb614adbc9ccd6fd2ea511ced5fba496b34
rabby159 <ablazerabbi159@gmail.com>
2025-04-10 14:52:55 +0600

    route define

---
+++ simple-book/src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>Status: 404</h1>
+            <p>Page not found</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- simple-book/src/main.jsx
+++ simple-book/src/main.jsx
@@ -3,11 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root.jsx";
+import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
+import Home from "./components/Home/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
   },
 ]);
 
