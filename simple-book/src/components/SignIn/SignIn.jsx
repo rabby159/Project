4956be9ed5436a251120531ae8dcef6bff9629b4
rabby159 <ablazerabbi159@gmail.com>
2025-04-25 23:29:32 +0600

    email password signup

--- simple-book/src/components/SignIn/SignIn.jsx
+++ simple-book/src/components/SignIn/SignIn.jsx
@@ -17,39 +17,41 @@ const SignIn = () => {
 
   return (
     <div>
-      <div className="my-20 ">
-        <div className="hero-content">
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <div className="card-body">
-              <fieldset className="fieldset">
-                <label className="fieldset-label">Email</label>
-                <input type="email" className="input" placeholder="Email" />
-                <label className="fieldset-label">Password</label>
-                <input
-                  type="password"
-                  className="input"
-                  placeholder="Password"
-                />
-                <div>
-                  <a className="link link-hover">Forgot password?</a>
-                </div>
-                <button
-                  onClick={handleWithGoogleInfo}
-                  className="btn btn-neutral"
-                >
-                  SignIn with Google
-                </button>
-                <button className="btn btn-info text-white mt-4">
-                  Sign In
-                </button>
-                <p className="text-center">
-                  Don't have an account? <a href="/signUp">SignUp</a>
-                </p>
-              </fieldset>
+      <form>
+        <div className="my-20 ">
+          <div className="hero-content">
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+              <div className="card-body">
+                <fieldset className="fieldset">
+                  <label className="fieldset-label">Email</label>
+                  <input type="email" className="input" placeholder="Email" />
+                  <label className="fieldset-label">Password</label>
+                  <input
+                    type="password"
+                    className="input"
+                    placeholder="Password"
+                  />
+                  <div>
+                    <a className="link link-hover">Forgot password?</a>
+                  </div>
+                  <button
+                    onClick={handleWithGoogleInfo}
+                    className="btn btn-neutral"
+                  >
+                    SignIn with Google
+                  </button>
+                  <button className="btn btn-info text-white mt-4">
+                    Sign In
+                  </button>
+                  <p className="text-center">
+                    Don't have an account? <a href="/signUp">SignUp</a>
+                  </p>
+                </fieldset>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
--- simple-book/src/components/SignUp/SignUp.jsx
+++ simple-book/src/components/SignUp/SignUp.jsx
@@ -1,38 +1,61 @@
 import React from "react";
 
 const SignUp = () => {
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const name = event.target.name.value;
+    console.log(name, email, password);
+  };
+
   return (
     <div>
-      <div className="my-20 ">
-        <div className="hero-content">
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <div className="card-body">
-              <fieldset className="fieldset">
-                <label className="fieldset-label">Name</label>
-                <input type="text" className="input" placeholder="Your Name" />
-                <label className="fieldset-label">Email</label>
-                <input type="email" className="input" placeholder="Email" />
-                <label className="fieldset-label">Password</label>
-                <input
-                  type="password"
-                  className="input"
-                  placeholder="Password"
-                />
-                <div>
-                  <a className="link link-hover">Forgot password?</a>
-                </div>
-                <button className="btn btn-neutral">SignUp with Google</button>
-                <button className="btn btn-info text-white mt-4">
-                  Sign Up
-                </button>
-                <p className="text-center">
-                  Already have an account? <a href="/signIn">SignIn</a>
-                </p>
-              </fieldset>
+      <form onSubmit={handleSignUp}>
+        <div className="my-20">
+          <div className="hero-content">
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+              <div className="card-body">
+                <fieldset className="fieldset">
+                  <label className="fieldset-label">Name</label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="input"
+                    placeholder="Your Name"
+                  />
+                  <label className="fieldset-label">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="input"
+                    placeholder="Email"
+                  />
+                  <label className="fieldset-label">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    className="input"
+                    placeholder="Password"
+                  />
+                  <div>
+                    <a className="link link-hover">Forgot password?</a>
+                  </div>
+                  <button className="btn btn-neutral">
+                    SignUp with Google
+                  </button>
+                  <button className="btn btn-info text-white mt-4">
+                    Sign Up
+                  </button>
+                  <p className="text-center">
+                    Already have an account? <a href="/signIn">SignIn</a>
+                  </p>
+                </fieldset>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
