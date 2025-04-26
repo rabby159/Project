8f7721518f6f67c96a67a3d4304ccbae8b6ea180
rabby159 <ablazerabbi159@gmail.com>
2025-04-27 00:34:32 +0600

    auth provider create

---
+++ simple-book/src/components/Provider/AuthProvider.jsx
@@ -0,0 +1,19 @@
+import React, { createContext } from 'react';
+
+const AuthContext = createContext(null)
+
+const AuthProvider = () => {
+
+    const authInfo = {
+        name: 'md rabby'
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+
+
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
