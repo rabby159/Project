d53bc5a32920e4e0eb1adcba62671ad03c98f122
rabby159 <ablazerabbi159@gmail.com>
2025-06-01 12:09:21 +0600

    folder structured and mainLayout declare

--- myChoice-job-portal/src/router/router.jsx
+++ myChoice-job-portal/src/router/router.jsx
@@ -2,11 +2,21 @@ import {
     createBrowserRouter,
     
 } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Error from "../pages/Error/Error";
+import Home from "../pages/Home/Home";
   
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <MainLayout></MainLayout>,
+        errorElement: <Error></Error>,
+        children: [
+            {
+              path: "/",
+              element: <Home></Home>
+          }
+      ]
     },
 ]);
   
