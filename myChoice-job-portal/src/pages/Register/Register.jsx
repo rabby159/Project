0a6b6cce1fa6634d51730f09f5896c2e74542887
rabby159 <ablazerabbi159@gmail.com>
2025-06-01 15:06:45 +0600

    handle Register and get input value

--- myChoice-job-portal/src/pages/Register/Register.jsx
+++ myChoice-job-portal/src/pages/Register/Register.jsx
@@ -4,26 +4,46 @@ import registerAnimation from "../../assets/register-lottie/register-lottie.json
 import BannerRegister from "./BannerRegister";
 
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, email, password);
+  };
+
   return (
     <div>
       <BannerRegister></BannerRegister>
-      <div className="hero mt-20 max-w-7xl mx-auto">
+      <div className="hero my-20 max-w-7xl mx-auto">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left w-96">
             <Lottie animationData={registerAnimation}></Lottie>
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-xl">
-            <form className="card-body">
+            <form onSubmit={handleRegister} className="card-body">
               <fieldset className="fieldset">
                 <label className="label">Name</label>
-                <input type="name" className="input" placeholder="Name" />
+                <input
+                  type="name"
+                  name="name"
+                  className="input"
+                  placeholder="Name"
+                />
                 <label className="label">Email</label>
-                <input type="email" className="input" placeholder="Email" />
+                <input
+                  type="email"
+                  name="email"
+                  className="input"
+                  placeholder="Email"
+                />
                 <label className="label">Password</label>
                 <input
                   type="password"
                   className="input"
                   placeholder="Password"
+                  name="password"
                 />
                 <div>
                   <a className="link link-hover">Forgot password?</a>
