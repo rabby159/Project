6506e36c98a4e395e8b6b4341f4bcd54d7b4f3ee
rabby159 <ablazerabbi159@gmail.com>
2025-06-03 12:47:28 +0600

    home page banner complete design

--- myChoice-job-portal/src/pages/Home/Banner.jsx
+++ myChoice-job-portal/src/pages/Home/Banner.jsx
@@ -11,35 +11,55 @@ const Banner = () => {
       }}
     >
       <div class="absolute inset-0 bg-gradient-to-r from-black/100 to-transparent"></div>
-      <div className="text-left text-white isolation: isolate">
-        <div className="">
-          <h1 className="mb-5 text-6xl font-bold">
+      <div className="text-left  isolation: isolate">
+        <div>
+          <h1 className="mb-5 text-6xl font-bold text-white">
             Search Between More <br /> Then{" "}
             <span className="text-blue-400 bg-blue-100 py-1 px-2 rounded-2xl text-5xl">
               50,000
             </span>{" "}
             Open Jobs.
           </h1>
-          <p className="mb-5 text-xl">
+          <p className="mb-5 text-xl text-white">
             Trending Jobs Keywords:
-            <span className="text-blue-400 mr-1">
-              {" "}
-              Web Designer{" "}
-            </span>
-            <span className="text-blue-400 mr-1">
-              {" "}
-              Web Developer{" "}
-            </span>
-            <span className="text-blue-400 mr-1">
-              {" "}
-              IOS Developer{" "}
-            </span>
-            <span className="text-blue-400 mr-1">
-              {" "}
-              Android Developer{" "}
-            </span>
+            <span className="text-blue-400 mr-1"> Web Designer </span>
+            <span className="text-blue-400 mr-1"> Web Developer </span>
+            <span className="text-blue-400 mr-1"> IOS Developer </span>
+            <span className="text-blue-400 mr-1"> Android Developer </span>
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <div className="join bg-base-200 p-2 rounded-3xl">
+            <div className="w-60 flex items-center border-r-1 border-r-blue-200 ml-4 mr-1">
+              <label>
+                <input
+                  className="p-2"
+                  type="text"
+                  placeholder="Search Keywords.."
+                />
+              </label>
+            </div>
+            <select className="join-item w-40 border-r-1 border-r-blue-200 mr-1">
+              <option disabled selected>
+                Location
+              </option>
+              <option>Dhaka</option>
+              <option>Barisal</option>
+              <option>Khulna</option>
+            </select>
+            <select className="join-item w-40 mr-1">
+              <option disabled selected>
+                Category
+              </option>
+              <option>Software Developer</option>
+              <option>React Developer</option>
+              <option>Java Developer</option>
+              <option>PHP Developer</option>
+            </select>
+            <div>
+              <button className="btn btn-info rounded-br-3xl rounded-tr-3xl text-white join-item w-32 text-xl">
+                Search
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
