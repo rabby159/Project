2bcdef7dc94b8ed58f7610455b15af3c8afc3d8b
rabby159 <ablazerabbi159@gmail.com>
2025-06-04 01:02:44 +0600

    radio button for job filter

--- myChoice-job-portal/src/pages/Home/JobPost.jsx
+++ myChoice-job-portal/src/pages/Home/JobPost.jsx
@@ -3,9 +3,19 @@ import React from "react";
 const JobPost = () => {
   return (
     <div className="max-w-7xl mx-auto flex justify-center my-20">
-      <div className="join join-vertical lg:join-horizontal">
-        <button className="btn join-item">Button</button>
-        <button className="btn join-item">Button</button>
+      <div className="join join-vertical lg:join-horizontal bg-blue-200 py-4 px-5 rounded">
+        <input
+          className="join-item btn text-xl"
+          type="radio"
+          name="options"
+          aria-label="Latest Job"
+        />
+        <input
+          className="join-item btn text-xl"
+          type="radio"
+          name="options"
+          aria-label="Recent Job"
+        />
       </div>
     </div>
   );
