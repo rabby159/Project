d08078fd6bb40f2e95673dfb05121a6aec72b290
rabby159 <ablazerabbi159@gmail.com>
2025-06-14 01:02:11 +0600

    job card design format ready

--- myChoice-job-portal/src/pages/Home/JobCard.jsx
+++ myChoice-job-portal/src/pages/Home/JobCard.jsx
@@ -3,27 +3,49 @@ import React from "react";
 const JobCard = () => {
   return (
     <div>
-      <div className="card bg-base-100 shadow-sm">
-        <figure className="px-10 pt-10">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-            className="rounded-xl"
-          />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+      <div className="card bg-base-100 shadow-sm px-5 py-5">
+        <div className="flex items-center gap-6">
+          <figure className="flex-1">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="flex-auto">
+            <h3>title</h3>
+            <p>location</p>
           </div>
+        </div>
+        <div className="py-5">
+          <h2 className="card-title">Position Name</h2>
+          <div className="flex items-center gap-5">
+            <p>Shift</p>
+            <p>Time</p>
+          </div>
+        </div>
+        <div className="pb-5">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+            ipsam!
+          </p>
+        </div>
+        <div className="pb-5">
+          <p>technologies</p>
+        </div>
+        <div className="flex justify-between items-center">
+                  <h2>Amount</h2>
+                  <button className="btn btn-primary">Buy Now</button>
         </div>
       </div>
     </div>
   );
 };
+
+{
+  /* <div className="card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div> */
+}
 
 export default JobCard;
