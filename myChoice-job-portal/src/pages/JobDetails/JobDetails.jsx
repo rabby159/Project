a7e14b1858bd77d840de5588f46df6267783eba3
rabby159 <ablazerabbi159@gmail.com>
2025-06-18 21:48:36 +0600

    tittle card design

--- myChoice-job-portal/src/pages/JobDetails/JobDetails.jsx
+++ myChoice-job-portal/src/pages/JobDetails/JobDetails.jsx
@@ -25,12 +25,22 @@ const JobDetails = () => {
           <div className="flex-3 p-5">
             <div className="hero border-1 rounded-2xl border-blue-100">
               <div className="hero-content flex-col lg:flex-row flex gap-5">
-                <div className="flex-1 border-r-1 border-blue-100">
-                  <img
-                    src={company_logo}
-                    className="max-w-sm rounded-lg shadow-2xl"
-                  />
-                  <p>{title}</p>
+                <div className="flex-2 flex justify-center border-r-1 border-blue-100">
+                  <div>
+                    <div className="flex justify-center">
+                      <img
+                        src={company_logo}
+                        className="max-w-sm rounded-lg shadow-2xl w-20"
+                      />
+                    </div>
+                    <p className="flex justify-center">{title}</p>
+                    <p className="flex justify-center">{location}</p>
+                    <div className="flex justify-center">
+                      <button className="btn btn-info text-white">
+                        Apply Now
+                      </button>
+                    </div>
+                  </div>
                 </div>
                 <div className="flex-3">
                   <h1 className="text-5xl font-bold">Box Office News!</h1>
