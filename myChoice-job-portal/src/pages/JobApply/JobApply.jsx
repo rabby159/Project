9fe77775bfdf01b7208afb812d57bfb15149d478
rabby159 <ablazerabbi159@gmail.com>
2025-06-20 19:17:26 +0600

    job apply page create and title added

--- myChoice-job-portal/src/pages/JobApply/JobApply.jsx
+++ myChoice-job-portal/src/pages/JobApply/JobApply.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import NavbarForAuth from '../Shared/NavbarForAuth';
+import React from "react";
+import NavbarForAuth from "../Shared/NavbarForAuth";
 
 const JobApply = () => {
-    return (
-        <div>
-            <NavbarForAuth></NavbarForAuth>
-            <h2>Job Apply Page...</h2>
+  return (
+    <div>
+      <NavbarForAuth></NavbarForAuth>
+      <div className="max-w-7xl mx-auto">
+        <div className="text-center my-10 space-y-4">
+          <h2 className="text-4xl font-bold">Job Apply Form</h2>
+          <p className="text-xl">Please Fill Out the Form Below to Submit Your Job Application!</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default JobApply;
