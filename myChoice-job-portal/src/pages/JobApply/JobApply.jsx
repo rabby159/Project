4a469dceea3830b7887638eadc54b0a1901667dc
rabby159 <ablazerabbi159@gmail.com>
2025-06-20 21:42:09 +0600

    apply button workable

--- myChoice-job-portal/src/pages/JobApply/JobApply.jsx
+++ myChoice-job-portal/src/pages/JobApply/JobApply.jsx
@@ -11,19 +11,32 @@ const JobApply = () => {
 
     const form = e.target;
     const firstName = form.firstName.value;
+    const lastName = form.lastName.value;
+    const email = form.email.value;
+    const phone = form.phone.value;
+    const linkedInUrl = form.linkedInUrl.value;
+    const githubUrl = form.githubUrl.value;
+    const resumeUrl = form.resumeUrl.value;
+    const portfolioUrl = form.portfolioUrl.value;
+    const coverLetter = form.coverLetter.value;
+    const file = form.file.value;
     console.log(firstName);
   };
   return (
     <div>
       <NavbarForAuth></NavbarForAuth>
       <div className="max-w-7xl mx-auto">
+        {/* Page tittle */}
         <div className="my-10 space-y-4 text-center">
           <h2 className="text-4xl font-bold">Job Apply Form</h2>
           <p className="text-xl">
             Please Fill Out the Form Below to Submit Your Job Application!
           </p>
         </div>
+
+        {/* Full form */}
         <form onSubmit={handleJobApplyForm}>
+          {/* input data part */}
           <div className="flex gap-10 mb-10">
             {/* Personal information section*/}
             <div className="flex-1 mt-10">
@@ -51,7 +64,7 @@ const JobApply = () => {
                     </label>
                     <input
                       type="text"
-                      name="last-name"
+                      name="lastName"
                       placeholder="Type here.."
                       className="input input-info"
                     />
@@ -75,8 +88,8 @@ const JobApply = () => {
                     </label>
                     <input
                       type="number"
-                      name="Phone"
-                      placeholder="phone"
+                      name="phone"
+                      placeholder="Phone"
                       className="input input-info"
                     />
                   </div>
@@ -89,7 +102,7 @@ const JobApply = () => {
                     </label>
                     <input
                       type="url"
-                      name="linkedIn-url"
+                      name="linkedInUrl"
                       placeholder="Type here.."
                       className="input input-info"
                     />
@@ -101,7 +114,7 @@ const JobApply = () => {
                     </label>
                     <input
                       type="url"
-                      name="github-url"
+                      name="githubUrl"
                       placeholder="Type here.."
                       className="input input-info"
                     />
@@ -113,7 +126,7 @@ const JobApply = () => {
                     </label>
                     <input
                       type="url"
-                      name="resume-url"
+                      name="resumeUrl"
                       placeholder="Type here.."
                       className="input input-info"
                     />
@@ -125,7 +138,7 @@ const JobApply = () => {
                     </label>
                     <input
                       type="url"
-                      name="portfolio-url"
+                      name="portfolioUrl"
                       placeholder="Type here..If have"
                       className="input input-info"
                     />
@@ -141,7 +154,7 @@ const JobApply = () => {
               </div>
               <div className="p-5 border border-blue-100">
                 <div className="space-y-10">
-                  <div className="">
+                  <div>
                     <label className="label">
                       <span className="label-text font-semibold">
                         Cover Letter
@@ -149,24 +162,30 @@ const JobApply = () => {
                     </label>
                     <textarea
                       placeholder="Type here.."
-                      name="cover-letter"
+                      name="coverLetter"
                       className="textarea textarea-info w-[580px]"
                     ></textarea>
                   </div>
                 </div>
-                <div>
+                <div className="my-5">
                   <div className="space-x-7">
                     <label className="label">
                       <span className="label-text font-semibold">
                         Other Document
                       </span>
                     </label>
-                    <input type="file" className="file-input file-input-lg" />
+                    <input
+                      type="file"
+                      name="file"
+                      className="file-input file-input-lg"
+                    />
                   </div>
                 </div>
                 <div>
                   <fieldset className="fieldset border-base-300 rounded-box w-64 border p-4">
-                    <legend className="fieldset-legend">Login options</legend>
+                    <legend className="fieldset-legend">
+                      How you find us?
+                    </legend>
                     <div className="flex gap-3">
                       <label className="label">
                         <input type="checkbox" className="checkbox" />
@@ -189,6 +208,7 @@ const JobApply = () => {
               </div>
             </div>
           </div>
+          {/* Apply part*/}
           <div>
             <div className="text-center mb-20 ">
               <button className="btn btn-info text-white">Apply</button>
