d17d798dd26aa7ca20f43f59fb49d3e501ced483
rabby159 <ablazerabbi159@gmail.com>
2025-06-20 17:35:25 +0600

    private route design

---
+++ myChoice-job-portal/src/router/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import AuthContext from "../context/AuthContext/AuthContext";
+import { Navigate, useLocation } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const location = useLocation();
+
+  if (loading) {
+    return <span className="loading loading-spinner text-info loading-xl"></span>;
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate to="/signIn" state={location.pathname}></Navigate>;
+};
+
+export default PrivateRoute;
