7a38b0b6d4c8b94364226eb709549ed79d1ee78f
rabby159 <ablazerabbi159@gmail.com>
2025-06-21 20:48:19 +0600

    application table add

---
+++ myChoice-job-portal/src/pages/MyApplication/MyAppTable.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MyAppTable = ({ app }) => {
+  return (
+    <div>
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Littel, Schaden and Vandervort</td>
+      </tr>
+    </div>
+  );
+};
+
+export default MyAppTable;
--- myChoice-job-portal/src/pages/MyApplication/MyApplication.jsx
+++ myChoice-job-portal/src/pages/MyApplication/MyApplication.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavbarForAuth from "../Shared/NavbarForAuth";
+import useAuth from "../../hooks/useAuth";
+import MyAppTable from "./MyAppTable";
 
 const MyApplication = () => {
+  const { user } = useAuth();
+  const [application, setApplication] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/job-application?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setApplication(data));
+  }, []);
   return (
-      <div>
-          <NavbarForAuth></NavbarForAuth>
-      <h2 className="text-3xl">My Application</h2>
+    <div>
+      <NavbarForAuth></NavbarForAuth>
+      <div className="max-w-7xl mx-auto my-5 overflow-x-auto">
+        <table className="table table-md">
+          <thead>
+            <tr>
+              <th>Tittle</th>
+              <th>Company</th>
+              <th>Location</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {application.map((app, index) => (
+              <MyAppTable key={index} app={app}></MyAppTable>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
