b1831c14511242338be3d455290dbc36c7b7e937
rabby159 <ablazerabbi159@gmail.com>
2025-06-23 21:05:19 +0600

    add new field on add job page

--- myChoice-job-portal/src/pages/AddJob/AddJob.jsx
+++ myChoice-job-portal/src/pages/AddJob/AddJob.jsx
@@ -23,27 +23,87 @@ const AddJob = () => {
               <div className="bg-gray-800 text-base-300 text-center text-2xl font-bold p-5 rounded-t-xl">
                 <h1>Personal Information</h1>
               </div>
-              <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 p-5 border border-blue-100">
-                {/* Job Tittle */}
-                <div className="fieldset">
-                  <label className="label">
-                    <span className="label-text">Job Tittle</span>
-                  </label>
-                  <input type="text" name="jobTittle" className="input" placeholder="Type Here.." />
-                </div>
-                {/* Company Name */}
-                <div className="fieldset">
-                  <label className="label">
-                    <span className="label-text">Company Name</span>
-                  </label>
-                  <input type="text" name="companyName" className="input" placeholder="Type here.." />
-                </div>
-                {/* Job Tittle */}
-                <div className="fieldset">
-                  <label className="label">
-                    <span className="label-text">Job Tittle</span>
-                  </label>
-                  <input type="text" className="input" placeholder="Title" />
+              <div className="p-5 border border-blue-100">
+                <div className="grid grid-cols-1 lg:grid-cols-3 ml-14">
+                  {/* Job Tittle */}
+                  <div className="fieldset">
+                    <label className="label">
+                      <span className="label-text">Job Title</span>
+                    </label>
+                    <input
+                      type="text"
+                      name="title"
+                      className="input"
+                      placeholder="Type Here.."
+                    />
+                  </div>
+                  {/* Company Name */}
+                  <div className="fieldset">
+                    <label className="label">
+                      <span className="label-text">Company Name</span>
+                    </label>
+                    <input
+                      type="text"
+                      name="company"
+                      className="input"
+                      placeholder="Type here.."
+                    />
+                  </div>
+                  {/* Category */}
+                  <div className="fieldset">
+                    <label className="label">
+                      <span className="label-text">Category</span>
+                    </label>
+                    <select defaultValue="Press to Select" name="category" className="select">
+                      <option disabled={true}>Press to Select</option>
+                      <option>Engineering</option>
+                      <option>Marketing</option>
+                      <option>Finance</option>
+                      <option>Teaching</option>
+                      <option>Management</option>
+                      <option>Data Science</option>
+                      <option>Design</option>
+                      <option>Development</option>
+                    </select>
+                  </div>
+                  {/* No of vacancy */}
+                  <div className="fieldset">
+                    <label className="label">
+                      <span className="label-text">No of vacancy</span>
+                    </label>
+                    <input
+                      type="number"
+                      name="vacancy"
+                      className="input"
+                      placeholder="Type Here.."
+                    />
+                  </div>
+                  {/* Experience */}
+                  <div className="fieldset">
+                    <label className="label">
+                      <span className="label-text">Experience</span>
+                    </label>
+                    <select defaultValue="Press to Select" name="experience" className="select">
+                      <option disabled={true}>Press to Select</option>
+                      <option>Fresher</option>
+                      <option>6 Month</option>
+                      <option>1 Year</option>
+                      <option>More than 2 years</option>
+                    </select>
+                  </div>
+                  {/* Job Type */}
+                  <div className="fieldset">
+                    <label className="label">
+                      <span className="label-text">Job Type</span>
+                    </label>
+                    <select defaultValue="Press to Select" name="jobType" className="select">
+                      <option disabled={true}>Press to Select</option>
+                      <option>Full-time</option>
+                      <option>Part-time</option>
+                      <option>Contractual</option>
+                      <option>Remote</option>
+                    </select>
+                  </div>
                 </div>
               </div>
             </div>
