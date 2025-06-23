c6adab5e0cd515ade8172e41d88aa0a3a8b1d039
rabby159 <ablazerabbi159@gmail.com>
2025-06-23 19:49:39 +0600

    add job page tittle added

--- myChoice-job-portal/src/pages/AddJob/AddJob.jsx
+++ myChoice-job-portal/src/pages/AddJob/AddJob.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import NavbarForAuth from "../Shared/NavbarForAuth";
 
 const AddJob = () => {
-    return (
-        <div>
-            <h3>Add Job page</h3>
+  return (
+    <div>
+      <NavbarForAuth></NavbarForAuth>
+      <div className="max-w-7xl mx-auto">
+        {/* Page tittle */}
+        <div className="my-10 space-y-4 text-center">
+          <h2 className="text-4xl font-bold">Add Job Form</h2>
+          <p className="text-xl">
+            Please Fill Out the Form Below to Submit Your Job Form!
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AddJob;
