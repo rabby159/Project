35e634973ae3ac2773812e63e2b7ae830ca33ab8
rabby159 <ablazerabbi159@gmail.com>
2025-11-13 09:55:40 +0600

    service section card animation

--- zippyGo/zippyGo-client/src/pages/Home/Services/Services.jsx
+++ zippyGo/zippyGo-client/src/pages/Home/Services/Services.jsx
@@ -1,32 +1,30 @@
 import { Clock, Key, Package, Shield, Truck } from "lucide-react";
 import React from "react";
 import ServicesCard from "./ServicesCard";
-import fDelivery from '../../../assets/Delivery.json'
 
 const Services = () => {
   const services = [
-  {
-    animation: fDelivery,
-    title: "Fast Delivery",
-    description: "Get your parcels delivered quickly with our reliable express service.",
-  },
-  {
-    animation: "https://lottie.host/your-animation2.json",
-    title: "Safe Packaging",
-    description: "We ensure secure and careful handling of all items.",
-  },
-  {
-    animation: "https://lottie.host/your-animation3.json",
-    title: "24/7 Support",
-    description: "Our support team is always available to assist you.",
-  },
-  {
-    animation: "https://lottie.host/your-animation4.json",
-    title: "Trusted Service",
-    description: "We guarantee safety, transparency, and trust for every delivery.",
-  },
-];
-
+    {
+      animation:
+        "https://lottie.host/414b6c48-0f7b-411b-8e02-74732631f30e/rOSgO2V3Rv.json",
+      title: "Fast Delivery",
+      description:
+        "Get your parcels delivered quickly with our reliable express service.",
+    },
+    {
+      title: "Safe Packaging",
+      description: "We ensure secure and careful handling of all items.",
+    },
+    {
+      title: "24/7 Support",
+      description: "Our support team is always available to assist you.",
+    },
+    {
+      title: "Trusted Service",
+      description:
+        "We guarantee safety, transparency, and trust for every delivery.",
+    },
+  ];
 
   return (
     <section id="services" className="mb-20">
--- zippyGo/zippyGo-client/src/pages/Home/Services/ServicesCard.jsx
+++ zippyGo/zippyGo-client/src/pages/Home/Services/ServicesCard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import Lottie from "lottie-react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const ServicesCard = ({ service }) => {
   const { animation, title, description } = service;
   return (
     <Card className="p-6 border border-orange-100 hover:shadow-lg hover:-translate-y-1 transition-all duration-300 rounded-2xl text-center max-w-7xl mx-auto">
       <CardHeader className="flex flex-col items-center gap-3">
-        <Lottie animationData={animation} loop autoplay className="w-28 h-28" />
+        <DotLottieReact src={animation} loop autoplay></DotLottieReact>
         <CardTitle className="text-lg font-semibold text-gray-900">
           {title}
         </CardTitle>
