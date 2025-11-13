9e603c41f9bf3b86b10975e5c371f919445901f5
rabby159 <ablazerabbi159@gmail.com>
2025-11-13 11:15:18 +0600

    our bands logo marquee complete

--- zippyGo/zippyGo-client/src/pages/Home/Brands/Brands.jsx
+++ zippyGo/zippyGo-client/src/pages/Home/Brands/Brands.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
+import dhl from "../../../assets/bandsLogo/dhl.png";
+import etsy from "../../../assets/bandsLogo/etsy.png";
+import amazon from "../../../assets/bandsLogo/amazon.png";
+import zalatan from "../../../assets/bandsLogo/zalatan.png";
+import flip from "../../../assets/bandsLogo/flip.png";
+import Ali from "../../../assets/bandsLogo/ali.png";
+import Door from "../../../assets/bandsLogo/door.png";
+import Revoult from "../../../assets/bandsLogo/revolut.png";
+import gru from "../../../assets/bandsLogo/grubhub.png";
 
 const Brands = () => {
   const logos = [
-    { src: "/logos/dhl.png", alt: "DHL" },
-    { src: "/logos/fedex.png", alt: "FedEx" },
-    { src: "/logos/ups.png", alt: "UPS" },
-    { src: "/logos/tnt.png", alt: "TNT" },
-    { src: "/logos/aramex.png", alt: "Aramex" },
-    { src: "/logos/amazon.png", alt: "Amazon" },
+    { src: [dhl], alt: "DHL" },
+    { src: [etsy], alt: "etsy" },
+    { src: [zalatan], alt: "UPS" },
+    { src: [flip], alt: "Flip" },
+    { src: [Ali], alt: "Ali" },
+    { src: [amazon], alt: "Amazon" },
+    { src: [Door], alt: "Door" },
+    { src: [gru], alt: "Door" },
+    { src: [Door], alt: "Door" },
+    { src: [Revoult], alt: "Revoult" },
   ];
 
   return (
@@ -17,20 +30,19 @@ const Brands = () => {
         <h1>Brands Love to Work With Us</h1>
       </div>
       <Marquee
-        pauseOnHover={true}
         gradient={false}
-        speed={40}
-        className="flex items-center"
+        speed={70}
+        className="flex items-center my-10"
       >
         {logos.map((logo, index) => (
           <div
             key={index}
-            className="mx-10 flex items-center justify-center grayscale hover:grayscale-0 transition-all duration-300"
+            className="mx-10 flex items-center justify-center hover:grayscale-0 transition-all duration-300"
           >
             <img
               src={logo.src}
               alt={logo.alt}
-              className="h-12 w-auto object-contain"
+              className="h-20 w-20 object-contain"
             />
           </div>
         ))}
